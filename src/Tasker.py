8fc7d249c58f0ae9514c965cd84b3c1402060d89
Ben Meier <benmeier42@gmail.com>
2013-04-28 10:37:22 +0200

    Added basic argparsing, listing of tasklists

--- src/Tasker.py
+++ src/Tasker.py
@@ -1,7 +1,20 @@
 # Tasker.py
 import argparse
 import os
+import glob
 from os import path
+
+class Tasker:
+
+    # Open a tasker in the given folder
+    def __init__(self, _folder):
+        self.folder = _folder
+        self.current_list = "default"
+
+    # List all of the tasklists in the folder
+    def listLists(self):
+        flist = glob.glob(path.join(self.folder, "*.tsk"))
+        print flist
 
 
 if __name__ == '__main__':
@@ -10,7 +23,19 @@ if __name__ == '__main__':
     home = path.expanduser("~")
     taskerfolder = path.join(home, ".tasker")
     if not path.exists(taskerfolder):
+        # Create it if it does not exist
         print 'Creating Tasker folder at: ' + taskerfolder
         os.makedirs(taskerfolder)
 
+    # Parser argument
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f","--file", help="The Tasklist file you want to open (*.tsk).")
 
+    args = parser.parse_args()
+
+    tasker = Tasker(taskerfolder)
+    tasker.listLists()
+
+
+
+    print args
